034c45889899032c98b344bfbacd129db3e9ef2b
cbesmhsyn96 <system.c.hsyn@gmail.com>
2025-06-14 01:51:55 +0300

    Notlar -- müşteri menüsü

--- ManavSepetTutari/src/Customer.java
+++ ManavSepetTutari/src/Customer.java
@@ -2,6 +2,17 @@ import java.util.HashMap;
 
 public class Customer {
     public static double toplam = 0;
+
+         /*
+        ____Müşteri İşlemleri____
+        1. Sepete ürün ekleme tamam
+        2. Sepetten ürün çıkarma
+        3. Toplam Tutar hesaplama tamam
+        1 ve 3 şuan birleşik. 2 eklemeye benzer toplam = toplam-fiyat*adet
+        ______1 2 ve 3 menü seçenekleri______
+         */
+
+
     public static double sepeteUrunEkle(HashMap<String,Double>urunler,String urunAdi,int urunAdedi){
         toplam = urunler.get(urunAdi)*urunAdedi+toplam;
         return toplam;
--- ManavSepetTutari/src/RunnerClass.java
+++ ManavSepetTutari/src/RunnerClass.java
@@ -4,11 +4,6 @@ import java.util.regex.Pattern;
 
 public class RunnerClass extends Admin{
     public static void main(String[] args) {
-        /*
-        Ürün kaydı yapma
-        Sepete ürün ekleme
-        Toplam Tutar hesaplama
-         */
         HashMap<String,Double> urunKayitlari = new LinkedHashMap<>();
         Admin admin = new Admin();
         boolean adminDevamStatus;
