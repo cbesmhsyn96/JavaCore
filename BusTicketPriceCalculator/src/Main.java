0cae9775b3dde97463a672510f942e149bbc15fc
cbesmhsyn96 <system.c.hsyn@gmail.com>
2025-06-28 13:49:19 +0300

    -- class structure is created --

--- BusTicketPriceCalculator/src/Main.java
+++ BusTicketPriceCalculator/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        /*
+        Java ile mesafeye ve durumlara göre otobüs bileti fiyatı hesaplayan programı yapın.
+        Kullanıcıdan;
+        Mesafe (KM),
+        yaş ve yolculuk tipi (Tek Yön, Gidiş-Dönüş) bilgileri
+        ile mesafe başına ücret 0,10 TL / km olarak alın.
+         */
     }
 }
