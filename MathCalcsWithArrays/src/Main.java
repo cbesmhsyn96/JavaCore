5a0c0c8584a13ca75be4d4b5e71a01d34d7453e5
cbesmhsyn96 <system.c.hsyn@gmail.com>
2025-07-11 15:22:12 +0300

    -- 34th-37th projects are initialized in this project --

---
+++ MathCalcsWithArrays/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //First commit detailed message with Turkish
+        /*
+        Bu projede dizileri kullanarak ortalama, max, min, tekrar eden sayılar bulan metotlar yazılacak.
+        Metotların özyinelemeli olabiliritesine göre özyineli yazılacak.
+        Sonrasında dizilerle ilgili başka metotlar/sınıflar eklenebilir.
+         */
+    }
+}
