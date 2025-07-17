36936ada0d6413a86f22f8096cc57b363c503f46
cbesmhsyn96 <system.c.hsyn@gmail.com>
2025-07-17 19:51:42 +0300

    -- 40th project - toString method implemented --

--- StudentInformationSystem/src/Main.java
+++ StudentInformationSystem/src/Main.java
@@ -3,11 +3,6 @@ import student.Student;
 public class Main extends Student {
     public static void main(String[] args) {
         /*
-        Student sınıfı,
-        öğrenci bilgilerini tutar ve bu bilgilere erişim ve güncelleme imkanı sağlar.
-
-        toString metodu,
-        öğrenci bilgilerini yazdırmak için kullanılır.
 
         StudentManagementSystem Sınıfı:
         Öğrenci yönetimi için temel işlevleri içerir: ekleme, listeleme, güncelleme ve silme.addStudent,
@@ -15,8 +10,15 @@ public class Main extends Student {
 
         main metodu, kullanıcıdan girdi alarak bu işlemleri gerçekleştirir ve sonsuz bir döngü içinde kullanıcıyı yönlendirir.
          */
-
+        System.out.println("---------------------------------------studentInfoAdd() metodu çalışıyor");
         studentInfoAdd();
+        System.out.println("---------------------------------------getStudentInfosById() metodu çalışıyor");
         getStudentInfosById();
+        System.out.println("---------------------------------------updateStudentInfoById() metodu çalışıyor");
+        updateStudentInfoById();
+        listStudentInfos();
+        System.out.println("---------------------------------------toString kullanımı");
+        System.out.println(new Student().toString());
+
     }
 }
--- StudentInformationSystem/src/student/Student.java
+++ StudentInformationSystem/src/student/Student.java
@@ -61,17 +61,24 @@ public class Student extends PatternValidation {
     protected static void studentInfoAdd(){
         int count =0;
         int boundCount;
-        String studentCount;
+        boolean status = false;
+        String studentCount="";
         do {
             do {
-                System.out.print("Bilgileri girilecek öğrenci sayısı :");
-                studentCount = scanner.nextLine();
+                if(count == 0){
+                    System.out.print("Bilgileri girilecek öğrenci sayısı :");
+                    studentCount = scanner.nextLine();
+                }
                 if(!intPattern.matcher(studentCount).matches()){
                     System.out.println("Lüften 0 ile başlamayan ve en az 1 rakamdan oluşan bir sayı girin...");
+                }else{
+                    status = true;
+                    break;
                 }
-            }while (!intPattern.matcher(studentCount).matches());
+            }while (!intPattern.matcher(studentCount).matches()&& !status);
             boundCount = Integer.parseInt(studentCount);
             do {
+                System.out.println((count+1)+". öğrencinin bilgileri");
                 setId();
                 setfullName();
                 setEmail();
@@ -82,7 +89,7 @@ public class Student extends PatternValidation {
             studentNames.put(getId(),getfullName());
             studentEmails.put(getId(),getEmail());
             count++;
-        }while (count< boundCount);
+        }while (count!=boundCount);
 
     }
 
@@ -110,60 +117,52 @@ public class Student extends PatternValidation {
         }
     }
 
-    protected static void updateStudentInfosById(){
+    protected static void updateStudentInfoById(){
         String idVal;
-        String updateValue;
-        if(studentNames.size()!=0){
-            System.out.println("Bilgileri güncellenebilecek öğrenci idleri");
-            for (String key : studentNames.keySet()){
-                System.out.println(key);
-            }
-            do {
-                System.out.print("Lütfen bilgileri güncellenecek öğrencinin idsini girin :");
-                idVal = scanner.nextLine();
-                if(studentNames.containsKey(idVal)){
-                    System.out.println(idVal + " idli öğrencinin bilgileri");
-                    System.out.println("id = "+Integer.parseInt(idVal));
-                    System.out.println("Tam ad = "+studentNames.get(idVal));
-                    System.out.println("Email = "+studentEmails.get(idVal));
-                    do {
-                        System.out.println("Güncelleme yapacağınız bilgiyi giriniz [Tam ad|Email]:");
-                        updateValue = scanner.nextLine();
-                        if(!updateValuePattern.matcher(updateValue).matches()){
-                            System.out.println("Lütfen geçerli bir değer giriniz...");
-                        }
-                    }while (!updateValuePattern.matcher(updateValue).matches());
+        String updatedField;
+        if(!studentEmails.isEmpty()){
+            System.out.print("Bilgileri güncellenecek öğrencinin idsi :");
+            idVal = scanner.nextLine();
+            if(studentEmails.keySet().contains(idVal)){
+                System.out.println(idVal+" idli öğrencinin güncellenebilecek bilgileri");
 
-                    switch (updateValue){
-                        case "Tam ad":updateFullName(idVal); break;
-                        case "Email":updateEmail(idVal);break;
+                System.out.println("Email = "+studentEmails.get(idVal));
+                System.out.println("Tam ad = "+studentNames.get(idVal));
+                //toString kullanılabilir
+                do {
+                    System.out.print("Güncellenecek alan [Email|Tam ad]:");
+                    updatedField = scanner.nextLine();
+                    if(!updateValuePattern.matcher(updatedField).matches()){
+                        System.out.println("Lütfen geçerli bir alan adı giriniz [Email|Tam ad]...");
                     }
-                /*
-                Burada switch case ile alınan değere göre Tam ad veya email değiştirilir.
-                id değiştirilmemeli
-                 */
-                }else{
-                    System.out.println("Lütfen listelenen idlerden birini yazınız...");
-                }
-            }while (!studentNames.containsKey(idVal));
+                }while (!updateValuePattern.matcher(updatedField).matches());
 
-        }else{
-            System.out.println("Öğrenci bilgi listesi boş. Lüften öğrenci bilgisi ekleyin.");
+                switch (updatedField){
+                    case "Email": updateEmail(idVal); break;
+                    case "Tam ad": updateFullName(idVal); break;
+                }
+
+            }
+
         }
     }
-
-    /*
-        Student sınıfı,
-        öğrenci bilgilerini tutar ve bu bilgilere erişim ve güncelleme imkanı sağlar.
-
-        toString metodu,(ayrı metot olarak yazacağım tüm idlerin bilgilerini listelerken kullanılacak.)
-        öğrenci bilgilerini yazdırmak için kullanılır.
-     */
+    protected static void listStudentInfos(){
+        System.out.println("Öğrencilerin Bilgileri");
+        for (Map.Entry entry : studentNames.entrySet()){
+            System.out.println("------------------------------------");
+            System.out.println("id = "+entry.getKey());
+            System.out.println("Tam ad = "+entry.getValue());
+            System.out.println("Email = "+studentEmails.get(entry.getKey().toString()));
+            System.out.println("------------------------------------");
+        }
+    }
 
     @Override
     public String toString() {
         //tüm idlerin bilgilerini listelerken kullanılacak
-        return "";
+        return "idler = ".concat(studentNames.keySet().toString()).
+                concat("\nEmailler = {"+studentEmails.values()+"}").
+                concat("\nTam adlar = {"+studentNames.values()+"}");
     }
 
     private static void updateFullName(String idVal){
@@ -179,6 +178,7 @@ public class Student extends PatternValidation {
             }
         }while (!fullNamePattern.matcher(newFullNameVal).matches()&&studentNames.get(idVal).equals(newFullNameVal));
         studentNames.replace(idVal,newFullNameVal);
+        System.out.println(idVal+" idli öğrencinin tam adı "+newFullNameVal+" ile güncellendi.");
     }
 
     private static void updateEmail(String idVal){
@@ -194,6 +194,7 @@ public class Student extends PatternValidation {
             }
         }while (!emailPattern.matcher(newEmailVal).matches()&&studentEmails.get(idVal).equals(newEmailVal));
         studentEmails.replace(idVal,newEmailVal);
+        System.out.println(idVal+" idli öğrencinin emaili "+newEmailVal+" ile güncellendi.");
     }
 
 
